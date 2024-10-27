3fd5d89ae14398c534df3ed430cc1909ca5005a2
Filipp Ozinov <filipp@bakanov.su>
2024-10-27 21:25:42 +0400

    Write logs to file, split by database (#9)

--- mysql_ch_replicator/main.py
+++ mysql_ch_replicator/main.py
@@ -2,6 +2,9 @@
 
 import argparse
 import logging
+from logging.handlers import RotatingFileHandler
+import sys
+import os
 
 from .config import Settings
 from .db_replicator import DbReplicator
@@ -10,15 +13,38 @@ from .monitoring import Monitoring
 from .runner import Runner
 
 
-def set_logging_config(tags):
+def set_logging_config(tags, log_file=None):
+
+    handlers = []
+    handlers.append(logging.StreamHandler(sys.stderr))
+    if log_file is not None:
+        handlers.append(
+            RotatingFileHandler(
+                filename=log_file,
+                maxBytes=50*1024*1024,  # 50 Mb
+                backupCount=3,
+                encoding='utf-8',
+                delay=False,
+            )
+        )
+
     logging.basicConfig(
         level=logging.INFO,
         format=f'[{tags} %(asctime)s %(levelname)8s] %(message)s',
+        handlers=handlers,
     )
 
 
 def run_binlog_replicator(args, config: Settings):
-    set_logging_config('binlogrepl')
+    if not os.path.exists(config.binlog_replicator.data_dir):
+        os.mkdir(config.binlog_replicator.data_dir)
+
+    log_file = os.path.join(
+        config.binlog_replicator.data_dir,
+        'binlog_replicator.log',
+    )
+
+    set_logging_config('binlogrepl', log_file=log_file)
     binlog_replicator = BinlogReplicator(
         settings=config,
     )
@@ -29,11 +55,29 @@ def run_db_replicator(args, config: Settings):
     if not args.db:
         raise Exception("need to pass --db argument")
 
-    set_logging_config(f'dbrepl {args.db}')
+    db_name = args.db
+
+    if not os.path.exists(config.binlog_replicator.data_dir):
+        os.mkdir(config.binlog_replicator.data_dir)
+
+    db_dir = os.path.join(
+        config.binlog_replicator.data_dir,
+        db_name,
+    )
+
+    if not os.path.exists(db_dir):
+        os.mkdir(db_dir)
+
+    log_file = os.path.join(
+        db_dir,
+        'db_replicator.log',
+    )
+
+    set_logging_config(f'dbrepl {args.db}', log_file=log_file)
 
     db_replicator = DbReplicator(
         config=config,
-        database=args.db,
+        database=db_name,
         target_database=getattr(args, 'target_db', None),
         initial_only=args.initial_only,
     )
